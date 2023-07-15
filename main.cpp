52a03631358c826cea3edc9f6bc643ad5a52e037
Kai <LINKAIZXC@GMAIL.COM>
2023-07-15 22:50:27 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <iostream>
+#include <string>
+
+Traffic_light& operator++(Traffic_light& t)
+{
+    using enum Traffic_light;
+    switch(t) {
+        case green: return t= yellow;
+        case yellow: return t= red;
+
+    }
+}
+
+
+
+int main() {
+    Vector v(6);
+    for (int i = 0 ; i < v.size(); i++) {
+        std::cin >> v[i];
+
+    }
+    double sum = 0;
+    for (int i =0; i < v.size(); i++) {
+        sum += v[i];
+    }
+
+    return 0;
+}
