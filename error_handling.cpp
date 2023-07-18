177004fa8e31ce206abac5a6351c4f1354dabe4d
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2023-07-19 00:56:56 +0800

    error handling cpp

---
+++ error_handling.cpp
@@ -0,0 +1,13 @@
+//
+// Created by admin on 2023-07-19.
+//
+// out_of_range
+
+// try {} catch {}
+// throw an error indicator when failure is normal
+// throw an exception when the error is rare
+// terminate when the error is unrecoverable
+// assert(condition && "message")
+// constexpr (evaluate something at compile time, not at run time)
+// static_asset(condition, message)
+// noexcept // only written for functions where no exceptions can ever be thrown
