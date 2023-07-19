335305b7cdb54304c13f101dd114c71521a6cf38
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2023-07-19 22:07:16 +0800

    encapsulation

---
+++ encapsulation.cpp
@@ -0,0 +1,22 @@
+//
+// Created by admin on 2023-07-19.
+// basically encapsulation is hiding ur class information inside private
+
+class Kai {
+private:
+    string name;
+    int age;
+    int height;
+
+public:
+    int getAge() return age;
+
+};
+
+
+
+int main() {
+    Kai me = Kai();
+    cout << me.getAge() << "\n";
+    return 0;
+}
